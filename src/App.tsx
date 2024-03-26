0bc00c580060858c29376c16965e273b94f6ddf1
root <williamdn@yahoo.com>
2024-03-25 22:58:46 -0300

    feat: add shadcnui and eslint and prettier config

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
+import './global.css'
+
+import { Button } from './components/ui/button'
 
 export function App() {
-
   return (
-    <h1>Hello Mundo</h1>
- 
+    <div className="flex h-screen flex-col flex-col flex-col items-center justify-center">
+      <Button>Button</Button>
+    </div>
   )
 }
-
